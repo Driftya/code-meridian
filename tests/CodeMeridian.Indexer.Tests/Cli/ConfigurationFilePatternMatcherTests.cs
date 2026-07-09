01f0c51217cf79adaa69a5b5cddda0ddac95c2d4
Josh Gomez <josh.gomez90@gmail.com>
2026-07-09 19:08:18 +0200

    test: expand config matcher coverage

--- tests/CodeMeridian.Indexer.Tests/Cli/ConfigurationFilePatternMatcherTests.cs
+++ tests/CodeMeridian.Indexer.Tests/Cli/ConfigurationFilePatternMatcherTests.cs
@@ -34,4 +34,39 @@ public sealed class ConfigurationFilePatternMatcherTests
 
         result.Should().BeTrue();
     }
+
+    [Fact]
+    public void IsConfigurationFile_IgnoresBlankPatterns()
+    {
+        var file = new FileInfo(@"C:\repo\appsettings.json");
+
+        var result = ConfigurationFilePatternMatcher.IsConfigurationFile(file, [" ", "\t"]);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsConfigurationFile_WithWildcardNotAnchoredAtStart_MatchesSuffixPattern()
+    {
+        var file = new FileInfo(@"C:\repo\docker-compose.sample.yaml");
+
+        var result = ConfigurationFilePatternMatcher.IsConfigurationFile(file, ["*sample.yaml"]);
+
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(@"C:\repo\appsettings.json", 0)]
+    [InlineData(@"C:\repo\appsettings.Development.json", 1)]
+    [InlineData(@"C:\repo\meridian.json", 2)]
+    [InlineData(@"C:\repo\meridian.sample.json", 2)]
+    [InlineData(@"C:\repo\custom.json", 3)]
+    [InlineData(@"C:\repo\.env", 4)]
+    [InlineData(@"C:\repo\docker-compose.yml", 5)]
+    public void GetOrder_ReturnsExpectedPriority(string path, int expectedOrder)
+    {
+        var result = ConfigurationFilePatternMatcher.GetOrder(new FileInfo(path));
+
+        result.Should().Be(expectedOrder);
+    }
 }
